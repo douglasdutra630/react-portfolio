1840fc8e21d179484db6ffbb40fd4a0679c22ef8
douglasdutra630 <douglas.dutra630@gmail.com>
2020-04-01 16:40:46 -0600

    Class vs Functional Components in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import PortfolioItem from "./portfolio-item";
+
 export default class PortfolioContainer extends Component {
     render(){
         return (
             <div>
                 <h2>Portfolio Items go here</h2>
+
+                <PortfolioItem/>
             </div>
         )
     }
