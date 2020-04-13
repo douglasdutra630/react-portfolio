e1eb2ac5a0b4ee570f21bb3ae314aee5d8558ccf
douglasdutra630 <douglas.dutra630@gmail.com>
2020-04-13 12:00:09 -0600

    adding styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,20 +8,23 @@ export default class NavigationContainer extends Component {
 
   render() {
     return (
-      <div>
-        <NavLink exact to="/" activeClassName="nav-link-active">
-          Home
-        </NavLink>
-        <NavLink to="/about-me" activeClassName="nav-link-active">
-          About
-        </NavLink>
-        <NavLink to="/contact" activeClassName="nav-link-active">
-          Contact
-        </NavLink>
-        <NavLink to="/blog" activeClassName="nav-link-active">
-          Blog
-        </NavLink>
-        {false ? <button>Add Blog</button> : null}
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+          {false ? <button>Add Blog</button> : null}
+        </div>
+        <div className="right-side">DOUG DUTRA</div>
       </div>
     );
   }
