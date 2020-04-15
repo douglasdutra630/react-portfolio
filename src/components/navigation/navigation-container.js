6362e7144168505eac67854403b6857789d518a1
douglasdutra630 <douglas.dutra630@gmail.com>
2020-04-15 16:58:53 -0600

    Finalizing the Logout Functionality with a Higher Order Component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,7 +1,10 @@
+  
 import React from "react";
+import axios from "axios";
+import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
 
-const NavigationContainer = (props) => {
+const NavigationContainer = props => {
   const dynamicLink = (route, linkText) => {
     return (
       <div className="nav-link-wrapper">
@@ -10,6 +13,21 @@ const NavigationContainer = (props) => {
         </NavLink>
       </div>
     );
+  };
+
+  const handleSignOut = () => {
+    axios
+      .delete("https://api.devcamp.space/logout", { withCredentials: true })
+      .then(response => {
+        if (response.status === 200) {
+          props.history.push("/");
+          props.handleSuccessfulLogout();
+        }
+        return response.data;
+      })
+      .catch(error => {
+        console.log("Error signing out", error);
+      });
   };
 
   return (
@@ -33,14 +51,19 @@ const NavigationContainer = (props) => {
           </NavLink>
         </div>
 
-        {props.loggedInStatus === "LOGGED_IN"
-          ? dynamicLink("/blog", "Blog")
-          : null}
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          dynamicLink("/blog", "Blog")
+        ) : null}
       </div>
 
-      <div className="right-side">JORDAN HUDGENS</div>
+      <div className="right-side">
+        DOUG DUTRA
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          <a onClick={handleSignOut}>Sign Out</a>
+        ) : null}
+      </div>
     </div>
   );
 };
 
-export default NavigationContainer;
+export default withRouter(NavigationContainer);
--- src/components/pages/no-match.js
+++ src/components/pages/no-match.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function() {
     return (
         <div>
-            <h2>We couldn't find a match</h2>
+            <h2>We couldn't find the page</h2>
             <Link to="/">Return to Homepage</Link>
         </div>
     )
