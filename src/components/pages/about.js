938c0267e38f1c6117d0edf5978c896554f2db66
douglasdutra630 <douglas.dutra630@gmail.com>
2020-04-29 16:10:09 -0600

    about & contact pages

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function() {
-  return <div>About</div>;
+import profilePicture from "../../../static/assets/images/profile/pfp.jpg";
+
+export default function () {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        <p>PLACEHOLDER TEXT HERE</p>
+      </div>
+    </div>
+  );
 }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,5 +1,26 @@
 import React from "react";
 
-export default function() {
-  return <div>Contact</div>;
+import contactPicture from "../../../static/assets/images/contact/vanilla.png";
+
+export default function () {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        <h2>Contact Info</h2>
+        <p>Name: Douglas Dutra (BrUch)</p>
+        <p>Email: douglas.dutra630@gmail.com</p>
+        <p>Phone Number: 1-801-382-5633</p>
+        <p>LinkedIn: blah</p>
+        <p>GitHub: Blah</p>
+      </div>
+    </div>
+  );
 }
